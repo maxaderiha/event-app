f797bef678f9cbf32b4decb5043a3295b00713d4
Maksim Adziaryaha <Maksim_Adziaryaha@epam.com>
2018-01-17 13:53:35 +0300

    add color's constants

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+//colors
+export const DARK_PRIMARY = '#455A64';
+export const LIGHT_PRIMARY = '#CFD8DC';
+export const PRIMARY = '#607D8B';
+export const TEXT_ICONS = '#FFFFFF';
+export const ACCENT = '#00BCD4';
+export const PRIMARY_TEXT = '#212121';
+export const SECONDARY_TEXT = '#757575';
+export const DIVIDER = '#BDBDBD';
